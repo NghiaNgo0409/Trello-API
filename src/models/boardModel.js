e69667ea112eca2a42e190b8346caf3362e9318a
NghiaNgo0409 <nghiango0409@gmail.com>
2024-08-15 11:34:01 +0700

    Build API get board detail

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -57,9 +57,23 @@ const findOneByID = async (id) => {
   }
 }
 
+const getDetails = async (id) => {
+  try {
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(id)
+      })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 export const boardModel = {
   BOARD_COLLECTION_NAME,
   BOARD_COLLECTION_SCHEMA,
   createNew,
-  findOneByID
+  findOneByID,
+  getDetails
 }
