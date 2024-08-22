dd95023e118b4fa8df78015d6bd93fedcccf792d
NghiaNgo0409 <nghiango0409@gmail.com>
2024-08-22 15:15:42 +0700

    Build api drag card in same column

--- src/routes/v1/columnRoute.js
+++ src/routes/v1/columnRoute.js
@@ -12,4 +12,6 @@ const Router = express.Router()
 
 Router.route('/').post(columnValidation.createNew, columnController.createNew)
 
+Router.route('/:id').put(columnValidation.update, columnController.update)
+
 export const columnRoutes = Router
